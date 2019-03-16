93af9edc6c98dc6424f59cca13e09dfa54c26fab
Usenov Aitemir Kamchybekovich <aitemir08@gmail.com>
2019-03-16 08:39:47 +0000

    [server] Migrate to ORM (closes #8) [server] Create tables with relationships for users (closes #9) [server] Add login page handling (closes #10) [client] Create login page (closes #15)

--- chirper.js
+++ chirper.js
@@ -1,42 +1,82 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const mysql = require('mysql')
-
+const Sequelize = require('sequelize')
 require('dotenv').config()
 
-const port = process.env.CHIRPER_PORT
+/* Настройка базы данных через ORM систему */
 
-const connection = mysql.createConnection({
-  host     : process.env.CHIRPER_DB_HOST,
-  user     : process.env.CHIRPER_DB_USER,
-  password : process.env.CHIRPER_DB_PASS,
-  database : process.env.CHIRPER_DB_NAME
+const sequelize = new Sequelize({
+  'host'    : process.env.CHIRPER_DB_HOST,
+  'port'     : process.env.CHIRPER_DB_PORT,
+  'username' : process.env.CHIRPER_DB_USER,
+  'password' : process.env.CHIRPER_DB_PASS,
+  'database' : process.env.CHIRPER_DB_NAME,
+  'dialect'  : 'mysql'
 })
+
+const User = sequelize.define('user', {
+  'login' : {
+    'type' : Sequelize.STRING,
+    'allowNull' : false,
+    'unique' : true
+  },
+  'password' : {
+    'type' : Sequelize.STRING,
+    'allowNull' : false,
+  } 
+})
+const Chirp = sequelize.define('chirp', {
+  'content': {
+      'type': Sequelize.STRING,
+      'allowNull': false
+  }
+})
+
+User.hasMany(Chirp)
+Chirp.belongsTo(User)
 
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended: true }))
 
+// Задаем обработчики путей (роутинг)
+
 app.get('/', (_, response) => {
-  connection.query('SELECT * FROM chirps;', (error, results) => {
-    if (error) throw error
-    
-    response.render('index', { 'chirps': results })
+  Chirp.findAll().then (results => {
+      response.render('index', { 'chirps': results })
+  }).catch(error => {
+      console.error(error)
+      response.status(500).end('Interval Server Error')
   })
 })
-
 app.post('/', (request, response) => {
   const content = request.body.content  
-  connection.query(`INSERT INTO chirps SET ?;`, { 'content': content }, (error) => {
-    if (error) throw error
-    
-    response.writeHead(302, {
-      'Location': '/'
-    })
-    response.end()
+  Chirp.create({ 
+    'content': content 
+  }),then(chirp => {
+      response.redirect('/')
+  }).catch(error => {
+      console.error(error)
+      response.status(500).end('Interval Server Error')
   })
 })
 
-app.listen(port, () => console.log(`The Chirper server is listening on port ${port}.`))
+app.get('/login', (request,response) => {
+    response.render('login')
+})
 
+app.post('/login', (request, response) =>{
+  
+})
+// Созданием структуру базы при помощи ORM и запускаем веб-сервер
+
+sequelize.sync().then(() => {
+    return User.create({
+      'login': 'user',
+      'password': process.env.CHIRPER_TEST_USER_PASS
+    })
+}).then(() => {
+  const port = process.env.CHIRPER_PORT
+  app.listen(port, () => console.log(`The Chirper server is listening on port ${port}.`))
+})
